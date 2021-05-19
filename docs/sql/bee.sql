6c3b38b9d62ed4053e746aee8807c21924bded39
xhrg <634789257@qq.com>
2021-05-20 00:26:40 +0800

    新增数据库脚本

--- docs/sql/bee.sql
+++ docs/sql/bee.sql
@@ -1,5 +1,55 @@
+/*
+ Navicat Premium Data Transfer
 
+ Source Server         : 127.0.0.1
+ Source Server Type    : MySQL
+ Source Server Version : 50734
+ Source Host           : localhost:3306
+ Source Schema         : bee
 
-create table bee_api{
+ Target Server Type    : MySQL
+ Target Server Version : 50734
+ File Encoding         : 65001
 
-}
+ Date: 20/05/2021 00:25:48
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for bee_api
+-- ----------------------------
+DROP TABLE IF EXISTS `bee_api`;
+CREATE TABLE `bee_api`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `note` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for bee_api_group
+-- ----------------------------
+DROP TABLE IF EXISTS `bee_api_group`;
+CREATE TABLE `bee_api_group`  (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '分组名称',
+  `note` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '分组描述',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for bee_router
+-- ----------------------------
+DROP TABLE IF EXISTS `bee_router`;
+CREATE TABLE `bee_router`  (
+  `id` int(11) NOT NULL,
+  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT 'http',
+  `target_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '目标地址',
+  `api_id` int(11) NULL DEFAULT NULL COMMENT 'api的id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
