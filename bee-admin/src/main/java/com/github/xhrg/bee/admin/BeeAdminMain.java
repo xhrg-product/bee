2463b4d0753cab6115919096e5a875022a63ce19
xhrg <634789257@qq.com>
2021-05-19 18:00:59 +0800

    整理main方法

---
+++ bee-admin/src/main/java/com/github/xhrg/bee/admin/BeeAdminMain.java
@@ -0,0 +1,11 @@
+package com.github.xhrg.bee.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeAdminMain {
+    public static void main(String[] args) {
+        SpringApplication.run(BeeAdminMain.class);
+    }
+}
---
+++ bee-basic/src/main/java/com/github/xhrg/bee/basic/Mapper.java
@@ -0,0 +1,4 @@
+package com.github.xhrg.bee.basic;
+
+public class Mapper {
+}
