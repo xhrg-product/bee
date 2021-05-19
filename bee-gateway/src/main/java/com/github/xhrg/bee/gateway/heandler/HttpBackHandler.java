1384ac2e39890da5fb460e36e1bd6147a90ff9c4
xhrg <634789257@qq.com>
2021-05-19 19:54:19 +0800

    bug fix

--- bee-gateway/src/main/java/com/github/xhrg/bee/gateway/heandler/HttpBackHandler.java
+++ bee-gateway/src/main/java/com/github/xhrg/bee/gateway/heandler/HttpBackHandler.java
@@ -3,11 +3,13 @@ package com.github.xhrg.bee.gateway.heandler;
 import com.github.xhrg.bee.gateway.api.Context;
 import com.github.xhrg.bee.gateway.netty.NettyRequestHandler;
 import com.github.xhrg.bee.gateway.router.HttpRouter;
+import io.netty.channel.ChannelHandler;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@ChannelHandler.Sharable
 @Component
 public class HttpBackHandler extends NettyRequestHandler {
 
