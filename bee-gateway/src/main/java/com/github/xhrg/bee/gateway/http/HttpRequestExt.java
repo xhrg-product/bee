14eb53a40370cb3444e2674ac0eacd301987d244
xhrg <634789257@qq.com>
2021-05-20 17:28:48 +0800

    增加http对象

---
+++ bee-gateway/src/main/java/com/github/xhrg/bee/gateway/http/HttpRequestExt.java
@@ -0,0 +1,13 @@
+package com.github.xhrg.bee.gateway.http;
+
+import io.netty.handler.codec.http.FullHttpRequest;
+import lombok.Data;
+
+@Data
+public class HttpRequestExt {
+
+    private FullHttpRequest fullHttpRequest;
+
+
+
+}
---
+++ bee-gateway/src/main/java/com/github/xhrg/bee/gateway/http/HttpResponseExt.java
@@ -0,0 +1,12 @@
+package com.github.xhrg.bee.gateway.http;
+
+import io.netty.handler.codec.http.FullHttpResponse;
+import lombok.Data;
+
+@Data
+public class HttpResponseExt {
+
+    private FullHttpResponse fullHttpResponse;
+
+
+}
