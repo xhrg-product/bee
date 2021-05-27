23c10ca8ba0c28eeb9c176966ca251ba75d9de0d
xhrg <634789257@qq.com>
2021-05-27 14:41:00 +0800

    完善接口

--- bee-admin/src/main/java/com/github/xhrg/bee/admin/controller/UserController.java
+++ bee-admin/src/main/java/com/github/xhrg/bee/admin/controller/UserController.java
@@ -5,6 +5,7 @@ import com.github.xhrg.bee.admin.util.ResponseUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,8 +21,10 @@ public class UserController {
     @RequestMapping("/info")
     public Object info(Map<String, Object> map) {
         Map<String, Object> returnMap = new HashMap<>();
-        returnMap.put("name", "mockName");
-        returnMap.put("avatar", "avatar");
+        returnMap.put("name", "mockName");//
+        returnMap.put("avatar", "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        returnMap.put("roles", Arrays.asList("editor"));
+        returnMap.put("introduction", "张三");
         return ResponseUtils.data(returnMap);
     }
 }
